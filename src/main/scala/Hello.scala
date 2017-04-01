4eb62541db7e010f02c2c419065fb9ec1c952948
santosh981433 <santoshb070@gmail.com>
2017-04-02 01:59:07 +0530

    C:\Users\bhami\scala_basics\

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -11,6 +11,15 @@ object Hello {
     println("length of first name is "+string.length)
     val name = string.concat(" bhamidipati")
     println("original name is "+name)
+
+    import org.apache.spark.{SparkContext,SparkConf}
+    val conf = new SparkConf().setAppName("spark-1").setMaster("local")
+    val sc = new SparkContext(conf)
+
+    val data = sc.parallelize(List(1,2,3,4))
+    val output=data.map(l=>l+1)
+    output.foreach(println)
+
   }
 
 }
