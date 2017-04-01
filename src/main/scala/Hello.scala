be758e686a47e2432573e9a2998f8e6eb8085c2b
santosh981433 <santoshb070@gmail.com>
2017-04-02 01:51:48 +0530

    C:\Users\bhami\scala_basics\src\main\scala

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -7,6 +7,10 @@ object Hello {
     val b=12
     val c = a*b+10
     println(c)
+    val string = "santosh"
+    println("length of first name is "+string.length)
+    val name = string.concat(" bhamidipati")
+    println("original name is "+name)
   }
 
 }
