7c7eb9d2a0e153827e8c7a5ab41af3e17f9a3ddf
santosh981433 <santoshb070@gmail.com>
2017-04-02 02:20:28 +0530

    C:\Users\bhami\scala_basics

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,26 +1,27 @@
 /**
-  * Created by bhami on 02/04/2017.
+  * Created by santosh on 02/04/2017.
   */
 object Hello {
   def main(args: Array[String]): Unit = {
-    val a=10
-    val b=12
-    val c = a*b+10
-    println(c)
-    val string = "santosh"
-    println("length of first name is "+string.length)
-    val name = string.concat(" bhamidipati")
-    println("original name is "+name)
+    val first_name="Santosh"
+    val last_name=" Bhamidipati"
+    val name = first_name.concat(last_name)
+    println(name)
 
+    //we can import packages where ever needed in scala there is no condition like it is needed to be given at the starting of the code
     import org.apache.spark.{SparkContext,SparkConf}
     val conf = new SparkConf().setAppName("spark-1").setMaster("local")
     val sc = new SparkContext(conf)
 
+    //parallelizing an existing collection in spark
     val data = sc.parallelize(List(1,2,3,4))
     val output=data.map(l=>l+1)
     output.foreach(println)
 
-    val textdata = sc.textFile("")
+    //choosing file from an existing location
+    val textdata = sc.textFile("Data/test_data_file")
+    val text_finder=textdata.map(l=>l.contains("santosh")).collect()
+    text_finder.foreach(println)
 
   }
 
